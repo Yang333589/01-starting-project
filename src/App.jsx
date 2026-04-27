6b9adf2c9980551b2f2016d86eead4aaca1cc73d
Monica Yang <1212monicayang@gmail.com>
2026-04-27 11:56:13 -0400

    Moved CoreConcepts into its own folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcepts/CoreConcept";
 import TabButton from "./components/TabButton/TabButton";
 import { useState } from "react";
+import CoreConcepts from "./components/CoreConcepts/CoreConcepts";
 
 
 // function UserData(){
@@ -38,16 +39,7 @@ function App() {
     <div>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Time to get started!</h2>
-          <ul>
-            {
-              CORE_CONCEPTS.map((conceptItem) => 
-                <CoreConcept key={conceptItem.title} {...conceptItem} />)
-              
-            }
-          </ul>
-        </section>
+       <CoreConcepts />
         <section id="examples">
           <h2>Examples</h2>
           <menu>
